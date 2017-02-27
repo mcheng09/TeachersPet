cd0067d1fc8d6d6aeca380cd07c9b1e656ea02d1
Michael Cheng <mcheng.oc@gmail.com>
2017-02-26 20:08:37 -0800

    Added a message if no lectures found

--- app/components/LectureList.jsx
+++ app/components/LectureList.jsx
@@ -5,6 +5,11 @@ var LectureList = React.createClass({
   render: function() {
     var {lectures} = this.props;
     var renderLectures = () => {
+      if (lectures.length === 0) {
+        return (
+          <p>No Lectures Available</p>
+        )
+      }
       return lectures.map((lecture) => {
         return (
           <Lecture key={lecture.id} {...lecture} />
